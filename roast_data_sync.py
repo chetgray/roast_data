cac69ff13fcdbc1d2f31f7df4d8e7a681ed7f094
Chet Gray <chetgray@gmail.com>
2018-06-27 17:57:42 -0400

    Begin sync utility script

---
+++ roast_data_sync.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Synchronize roast database with latest roast logs.")
+
+    args = parser.parse_args()
+    main()
