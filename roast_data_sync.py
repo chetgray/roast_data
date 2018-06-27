45bba1e72a85fc7eb5e9cf5f17b3f2ad7a4faf04
Chet Gray <chetgray@gmail.com>
2018-06-27 19:36:17 -0400

    Reorder main parameters (better now than later)

--- roast_data_sync.py
+++ roast_data_sync.py
@@ -9,7 +9,7 @@ from httplib2 import Http
 import oauth2client as oa
 
 
-def main(secret_path='client_secret.json', db_path='roast_data.sqlite',
+def main(db_path='roast_data.sqlite', secret_path='client_secret.json',
          after_date=datetime.date.min.strftime('%Y/%m/%d')):
     # From https://developers.google.com/gmail/api/quickstart/python
     SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
@@ -23,13 +23,14 @@ def main(secret_path='client_secret.json', db_path='roast_data.sqlite',
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Synchronize roast database with latest roast logs.")
-    parser.add_argument('--secret', dest='secret_path', default='client_secret.json',
-                        help="OAuth client ID file")
     parser.add_argument('--database', dest='db_path', default='roast_data.sqlite',
                         help="SQLite database file")
+    parser.add_argument('--secret', dest='secret_path', default='client_secret.json',
+                        help="OAuth client ID file")
     parser.add_argument('--after', dest='after_date',
                         default=datetime.date.min.strftime('%Y/%m/%d'),
                         type=lambda s: dateparser.parse(s).strftime('%Y/%m/%d'))
 
     args = parser.parse_args()
-    main(secret_path=args.secret_path, db_path=args.db_path, after_date=args.after_date)
+    main(db_path=args.db_path, secret_path=args.secret_path,
+         after_date=args.after_date)
