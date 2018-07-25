4b58fd716a527dac0d7efb4d06db0c96ee04d4d8
Chet Gray <chetgray@gmail.com>
2018-07-25 19:24:13 -0400

    Refactor e-mail syncing to method

--- roast_data_sync.py
+++ roast_data_sync.py
@@ -33,6 +33,57 @@ def import_csvs(con, csv_files):
     raise NotImplementedError
 
 
+def sync_emails(con, secret_path, after_date, before_date):
+    # From https://developers.google.com/gmail/api/quickstart/python
+    SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+    store = oafile.Storage('credentials.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = oaclient.flow_from_clientsecrets(secret_path, SCOPES)
+        creds = oatools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http(cache=".cache")))
+    message_resource = service.users().messages()
+    list_request = message_resource.list(userId='me',
+                                         q='from:lsr@smartroaster.com '
+                                           'has:attachment '
+                                           f'after:{after_date} '
+                                           f'before:{before_date}')
+    while list_request is not None:
+        list_response = list_request.execute()
+        for message in list_response['messages']:
+            message_id = message['id']
+            print(message_id, end=' ')
+            row = con.execute('SELECT internal_date'
+                              '  FROM message'
+                              '  WHERE id == ?',
+                              (message_id,)).fetchone()
+            if row is not None:
+                print(f"{row[0]} exists")
+                continue
+            full_message = message_resource.get(userId='me', id=message_id).execute()
+            # Fun little use of __next__ on a filtered generator
+            attachment_id = next((part for part in full_message['payload']['parts'] if part['filename']), None)['body']['attachmentId']
+            internal_date = datetime.datetime.fromtimestamp(int(full_message['internalDate'])/1000)
+            print(f"{internal_date} downloading...")
+            con.execute('INSERT INTO message (id, snippet, internal_date, attachment_id) '
+                        'VALUES (?, ?, ?, ?)',
+                        (message_id,
+                         full_message['snippet'],
+                         internal_date,
+                         attachment_id))
+            attachment = message_resource.attachments().get(
+                userId='me',
+                messageId=message_id,
+                id=attachment_id).execute()
+            data = urlsafe_b64decode(attachment['data']).decode()
+            con.execute('INSERT INTO message_data (message_id, data) '
+                        'VALUES (?, ?)',
+                        (message_id, data))
+            con.commit()
+        else:
+            list_request = message_resource.list_next(list_request, list_response)
+
+
 def main(db_path='roast_data.sqlite', secret_path='client_secret.json',
          after_date=datetime.date.min.strftime('%Y/%m/%d'),
          before_date=datetime.date.max.strftime('%Y/%m/%d'),
@@ -43,54 +94,7 @@ def main(db_path='roast_data.sqlite', secret_path='client_secret.json',
     if csv_files:
         import_csvs(con, csv_files)
     else:
-        # From https://developers.google.com/gmail/api/quickstart/python
-        SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-        store = oafile.Storage('credentials.json')
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = oaclient.flow_from_clientsecrets(secret_path, SCOPES)
-            creds = oatools.run_flow(flow, store)
-        service = build('gmail', 'v1', http=creds.authorize(Http(cache=".cache")))
-        message_resource = service.users().messages()
-        list_request = message_resource.list(userId='me',
-                                             q='from:lsr@smartroaster.com '
-                                               'has:attachment '
-                                               f'after:{after_date} '
-                                               f'before:{before_date}')
-        while list_request is not None:
-            list_response = list_request.execute()
-            for message in list_response['messages']:
-                message_id = message['id']
-                print(message_id, end=' ')
-                row = con.execute('SELECT internal_date'
-                                  '  FROM message'
-                                  '  WHERE id == ?',
-                                  (message_id,)).fetchone()
-                if row is not None:
-                    print(f"{row[0]} exists")
-                    continue
-                full_message = message_resource.get(userId='me', id=message_id).execute()
-                # Fun little use of __next__ on a filtered generator
-                attachment_id = next((part for part in full_message['payload']['parts'] if part['filename']), None)['body']['attachmentId']
-                internal_date = datetime.datetime.fromtimestamp(int(full_message['internalDate'])/1000)
-                print(f"{internal_date} downloading...")
-                con.execute('INSERT INTO message (id, snippet, internal_date, attachment_id) '
-                            'VALUES (?, ?, ?, ?)',
-                            (message_id,
-                             full_message['snippet'],
-                             internal_date,
-                             attachment_id))
-                attachment = message_resource.attachments().get(
-                    userId='me',
-                    messageId=message_id,
-                    id=attachment_id).execute()
-                data = urlsafe_b64decode(attachment['data']).decode()
-                con.execute('INSERT INTO message_data (message_id, data) '
-                            'VALUES (?, ?)',
-                            (message_id, data))
-                con.commit()
-            else:
-                list_request = message_resource.list_next(list_request, list_response)
+        sync_emails(con, secret_path, after_date, before_date)
 
 
 if __name__ == '__main__':
